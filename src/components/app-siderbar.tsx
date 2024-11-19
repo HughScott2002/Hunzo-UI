5cb7e555f2e7d2dcd3b94f932b080f487baed93d
hugh <hughscott2002@yahoo.com>
2024-11-18 20:58:51 -0500

    Changed Recipients to Contacts

--- src/components/app-siderbar.tsx
+++ src/components/app-siderbar.tsx
@@ -64,8 +64,8 @@ const data = {
       icon: ArrowRightLeft,
     },
     {
-      title: "Recipients",
-      url: "/recipients",
+      title: "Contacts",
+      url: "/contacts",
       icon: Contact,
     },
     {
