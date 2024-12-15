fcfb96377df0dbb326e0607dc6988353e009ecf2
Hugh Scott <hughscott2002@yahoo.com>
2024-12-15 15:32:23 -0500

    Managing the Layout

--- src/app/(root)/help/page.tsx
+++ src/app/(root)/help/page.tsx
@@ -3,7 +3,13 @@ import HunzoInactiveDevelopment from "@/components/HunzoInactiveDevelopment";
 const page = () => {
   return (
     <section className="w-full h-full">
-      <HunzoInactiveDevelopment />
+      <div className="flex h-full gap-2">
+        <div className="w-[80%]">
+          <HunzoInactiveDevelopment />
+        </div>
+        <div className="bg-hunzo-text-grey w-1 h-full rounded-full" />
+        <HunzoInactiveDevelopment />
+      </div>
     </section>
   );
 };
--- src/app/(root)/settings/page.tsx
+++ src/app/(root)/settings/page.tsx
@@ -2,7 +2,14 @@ import HunzoInactiveDevelopment from "@/components/HunzoInactiveDevelopment";
 import React from "react";
 
 const page = () => {
-  return <HunzoInactiveDevelopment />;
+  return (
+    <div className="size-full">
+      <div className="flex size-full border-t-2">
+        <div className="w-[50%] border-r-2"></div>
+        <div className="w-full"></div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
