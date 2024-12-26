0660cf332f790324cabe7982fb2af51984cdd5d8
Hugh Scott <hughscott2002@yahoo.com>
2024-12-26 13:28:31 -0500

    Tests and stuff

--- src/app/(root)/fx/page.tsx
+++ src/app/(root)/fx/page.tsx
@@ -1,9 +1,18 @@
+import { HunzoBigBarChart } from "@/components/HunzoBigBarChart";
+import CandlestickChart from "@/components/HunzoCandlestick";
 import HunzoInactiveDevelopment from "@/components/HunzoInactiveDevelopment";
 
 const page = () => {
   return (
     <section className="w-full h-full">
-      <HunzoInactiveDevelopment />
+      {/* <HunzoInactiveDevelopment /> */}
+      <div className="size-full flex  flex-col justify-start items-center rounded-3xl overflow-hidden">
+        <div className="w-full h-40 bg-hunzo-yellow">Hey</div>
+        <div className="w-full h-[60%]">
+          {/* <CandlestickChart /> */}
+          <HunzoBigBarChart />
+        </div>
+      </div>
     </section>
   );
 };
